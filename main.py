25657dbb39b2a41eb77a95468e50c4b0e82ec502
MecaCho <1099833328@qq.com>
2019-01-31 01:38:48 +0800

    add regions;

--- main.py
+++ main.py
@@ -22,20 +22,21 @@ def read_config():
 if __name__ == '__main__':
     regions = read_config()
     users = read_config()
-    usr = users[0].get("user")
-    pwd = getpass.getpass("please input user {} password : ".format(usr))
-    regions = users[0].get("regions")
-    region = regions[0]
-    project_id = region["project_id"]
-    url = region["ief_url"]
-    api_version = region["api_version"]
-    client = node.Node(
-            usr=usr, pwd=pwd, project_id=project_id,
-            url=url, api_version=api_version)
-    client.list_nodes()
-    client = tag.TAG(usr=usr, pwd=pwd, project_id=project_id, url=url, api_version=api_version)
-    client.query_ins(resource_type="edge_node", action="filter", tags=[{"key": "qwq", "values": []}])
-    print "0"*100
-    client.test_query_ins()
-    print "1"*100
-    client.test_batch_tags()
+    for user_info in users:
+        usr = user_info.get("user")
+        pwd = getpass.getpass("please input user {} password : ".format(usr))
+        regions = user_info.get("regions")
+        for region in regions:
+            project_id = region["project_id"]
+            url = region["ief_url"]
+            api_version = region["api_version"]
+            client = node.Node(
+                    usr=usr, pwd=pwd, project_id=project_id,
+                    url=url, api_version=api_version)
+            client.list_nodes()
+            client = tag.TAG(usr=usr, pwd=pwd, project_id=project_id, url=url, api_version=api_version)
+            client.query_ins(resource_type="edge_node", action="filter", tags=[{"key": "qwq", "values": []}])
+            print "0"*100
+            client.test_query_ins()
+            print "1"*100
+            client.test_batch_tags()
