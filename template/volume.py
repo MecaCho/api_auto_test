b75ec546333db7f6a9496c1d3b53ee952e2f3924
MecaCho <1099833328@qq.com>
2019-04-22 23:52:08 +0800

    fix json;

--- template/volume.py
+++ template/volume.py
@@ -1,63 +1,63 @@
 
 configmaps = [
                         {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-1",
                 "destination":"/tmp1",
                 "type":"configMap",
                 "name":"config1"
             },
                         {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-2",
                 "destination":"/tmp2",
                 "type":"configMap",
                 "name":"config2"
             },
                                     {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-3",
                 "destination":"/tmp3",
                 "type":"configMap",
                 "name":"config3"
             },
                                     {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-4",
                 "destination":"/tmp4",
                 "type":"configMap",
                 "name":"config4"
             },
                                     {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-5",
                 "destination":"/tmp5",
                 "type":"configMap",
                 "name":"config5"
             },
                                     {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-6",
                 "destination":"/tmp6",
                 "type":"configMap",
                 "name":"config6"
             },
                                     {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-7",
                 "destination":"/tmp7",
                 "type":"configMap",
                 "name":"config7"
             },
                                                 {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-8",
                 "destination":"/tmp8",
                 "type":"configMap",
                 "name":"config8"
             },
                                                 {
-                "read_only":true,
+                "read_only":True,
                 "source":"qwq-0422-9",
                 "destination":"/tmp9",
                 "type":"configMap",
